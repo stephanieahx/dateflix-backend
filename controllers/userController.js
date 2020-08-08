d4ff1933787af88d9575893c260efe9939976dfa
stephanieahx <stephanieanghs@gmail.com>
2020-08-08 12:08:33 +0800

    set up CRUD functions in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const userRepository = require('../repositories/userRepository');
 const sessionController = require('./sessionController');
 
 module.exports = {
+   // Register a new user
     async create(req, res) {
         try {
             // validate(req.body);
@@ -16,11 +17,13 @@ module.exports = {
     //     res.render('users/registration'); //res.redirect('users/registration')
     // },
 
+    // View all users
     async getAll(req, res) {
         const users = await userRepository.get();
         res.json("Viewing all users.")
     },
 
+    // Update user profile
     async update(req, res) {
         try {
             const { result } = await userRepository.update(req.params.id, req.body); // assigns the value of a key to a variable
@@ -35,6 +38,7 @@ module.exports = {
         }
     },
 
+    // Delete user profile 
     async delete(req, res) {
         try {
             const id = await userRepository.delete(req.params.id);
