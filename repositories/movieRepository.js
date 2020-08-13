0c0577cbc3fc9ef5f8432bd60fc7d55881f720a6
stephanieahx <stephanieanghs@gmail.com>
2020-08-13 19:18:02 +0800

    troubleshooting

--- repositories/movieRepository.js
+++ repositories/movieRepository.js
@@ -18,33 +18,6 @@ module.exports = {
         }
     },
 
-    // // View all movies from dateflix database as posters 
-    // async getAll() {
-    //     // all movie data
-    //     const allMovieData = await db.movies.find().toArray();
-
-    //     // // * TO REMOVE * RETURN ALL MOVIE DATA
-    //     // // return allMovieData;
-    //     console.log(allMovieData[0].Poster);
-    //     console.log(allMovieData[0]._id);
-    //     console.log(allMovieData[0].Title);
-
-    //     // // Get Title, _id and Poster key-value pairs
-    //     let movieList = [];
-    //     for (let i = 0; i < allMovieData.length; i++) { // sequence matters 
-    //         let movieData = {
-    //             "_id": allMovieData[i]._id,
-    //             "Poster": allMovieData[i].Poster,
-    //             "Title": allMovieData[i].Title,
-    //         };
-    //         console.log(movieData);
-    //         movieList.push(movieData);
-    //     }
-    //     console.log(movieList);
-    //     return movieList;
-
-    // },
-
     // View all popular movies from TMDB
     async getAllTMDB(req, res) {
         const response = await fetch(url);
